7fce9b72c98c4222b00db5994e02c432b45f91d4
anlop31 <analopez31@correo.ugr.es>
2023-10-26 14:19:36 +0200

    reader medio hecho

--- DBA_P2/src/dba_p2/mapReader.java
+++ DBA_P2/src/dba_p2/mapReader.java
@@ -20,26 +20,56 @@ public class mapReader {
     
     ArrayList<ArrayList<Integer>> matriz_mapa;
     
+    int contador = 0;
+    int num_filas = 0;
+    int num_columnas = 0;
+    
+    Mapa mapa;
+    
     public void read(String nombreArchivo){
         
         try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
-             while ((linea = br.readLine()) != null) { // lee la linea
-                ArrayList<Integer> fila = new ArrayList<>();
-                for (int i = 0; i < linea.length(); i++) { // cada elemento de la linea
-                    char c = linea.charAt(i);
-                    int valor = Character.getNumericValue(c);
-                    fila.add(valor); // lo añade a fila
+            while ((linea = br.readLine()) != null) { // lee la linea
+                switch (contador) {
+                    case 0:
+                        {
+                            // lee filas
+                            char c = linea.charAt(0);
+                            num_filas = Character.getNumericValue(c);
+                            contador++;
+                            break;
+                        }
+                    case 1:
+                        {
+                            // lee columnas
+                            char c = linea.charAt(0);
+                            num_columnas = Character.getNumericValue(c);
+                            contador++;
+                            break;
+                        }
+                    default:
+                        for (int i = 0; i < num_columnas; i++) { // cada elemento de la linea
+                            ArrayList<Integer> fila = new ArrayList<>();
+                            
+                            char c = linea.charAt(i);
+                            int valor = Character.getNumericValue(c);
+                            fila.add(valor); // lo añade a fila
+                            
+                            matriz_mapa.add(fila); // añade fila a la matriz
+                        }   break;
                 }
-                matriz_mapa.add(fila); // añade fila a la matriz
-            }
+                
+            } // fin while
         } catch (IOException e) {
             e.printStackTrace();
         }
         
         // construir mapa con lo leído
-        Mapa mapa = new Mapa(matriz_mapa);
-        
+        mapa = new Mapa(num_filas, num_columnas, matriz_mapa);
+    }
+    
+    Mapa getMapa(){
         return mapa;
     }
 }
