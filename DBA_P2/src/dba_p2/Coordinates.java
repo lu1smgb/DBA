c7d36efdf873bd7b19efa5f961624415c689f6e9
jepalfer <jepalfer2000@gmail.com>
2023-11-05 16:51:50 +0100

    Añadido override a método equals para comparaciones

--- DBA_P2/src/dba_p2/Coordinates.java
+++ DBA_P2/src/dba_p2/Coordinates.java
@@ -18,4 +18,15 @@ public class Coordinates {
         this.x = x;
         this.y = y;
     }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Coordinates otroPunto = (Coordinates) obj;
+        return x == otroPunto.x && y == otroPunto.y;
+    }
 }
