cb183d8dea6a5d85cef4a149a65f0de526cba6fa
lu1smgb <lu1smgb.gacc@gmail.com>
2023-11-09 16:45:15 +0000

    Actualizados constructores

--- DBA_P2/src/dba_p2/Entorno.java
+++ DBA_P2/src/dba_p2/Entorno.java
@@ -13,9 +13,17 @@ public class Entorno {
     Entorno(Mapa mapa) {
 
         this.mapa = mapa;
-        this.posicionAgente = new Coordinates(this.mapa.getNumberOfCols() - 1, this.mapa.getNumberOfRows() - 1);
-        this.posicionObjetivo = new Coordinates(0, 0);
+        this.posicionAgente = new Coordinates(0, 0);
+        this.posicionObjetivo = new Coordinates(this.mapa.getNumberOfCols() - 1, this.mapa.getNumberOfRows() - 1);
 
+    }
+
+    Entorno(Mapa mapa, Coordinates posicionAgente, Coordinates posicionObjetivo) {
+        
+        this.mapa = mapa;
+        this.posicionAgente = posicionAgente;
+        this.posicionObjetivo = posicionObjetivo;
+        
     }
 
     /**
@@ -91,38 +99,6 @@ public class Entorno {
         }
         return str;
 
-    }
-
-    public static void main(String args[]) {
-        // Array bidimensional 7x5
-        int num_rows = 7;
-        int num_cols = 5;
-        // ***
-        // ArrayList<ArrayList<Celda>> datos = new ArrayList<ArrayList<Celda>>();
-        int datos[][] = new int[num_rows][num_cols];
-        // ***
-        for (int i = 0; i < num_rows; i++) {
-            // ArrayList<Celda> row = new ArrayList<Celda>();
-            for (int j = 0; j < num_cols; j++) {
-                if (i != 0 && j != 0) {
-                    datos[i][j] = -1;
-                    // row.add(-1);
-                } else {
-                    datos[i][j] = 0;
-                    // row.add(0);
-                }
-            }
-            // datos.add(row);
-        }
-
-        // Instanciacion del mapa con nuevas dimensiones
-        try {
-            Mapa m = new dba_p2.Mapa("Mi mapa", 10, 12, datos);
-            Entorno e = new Entorno(m);
-            System.out.println(e.mapa);
-        } catch (Exception e) {
-            throw e;
-        }
     }
 
 }
