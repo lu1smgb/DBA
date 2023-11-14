3d73166f55252cb688fc1fa05b3f9c0fa3fbcbb1
lu1smgb <lu1smgb.gacc@gmail.com>
2023-11-14 12:34:51 +0000

    Agregados valores enteros a enum Movimiento

--- DBA_P2/src/dba_p2/Movimiento.java
+++ DBA_P2/src/dba_p2/Movimiento.java
@@ -2,9 +2,19 @@ package dba_p2;
 
 public enum Movimiento {
     
-    ARRIBA,
-    ABAJO,
-    IZQUIERDA,
-    DERECHA
+    ARRIBA(0),
+    ABAJO(1),
+    IZQUIERDA(2),
+    DERECHA(3);
+
+    private int value;
+
+    Movimiento(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
 
 }
