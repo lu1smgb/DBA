acd218e30fd15a2db39539ab25e130329d514b0a
lu1smgb <lu1smgb.gacc@gmail.com>
2023-12-14 12:43:02 +0000

    Comunicacion con performativa estandar

--- P3/tutorial/EnviarMensaje.java
+++ P3/tutorial/EnviarMensaje.java
@@ -8,10 +8,14 @@ public class EnviarMensaje extends SimpleBehaviour {
 
     private AgenteEmisor agente;
     private int paso;
+    private String idConversacion;
+    private boolean finalizado;
     
     EnviarMensaje(AgenteEmisor agente) {
         this.agente = agente;
         this.paso = 0;
+        this.idConversacion = "identificador-conversacion";
+        this.finalizado = false;
     }
 
     private void envioSimple() {
@@ -29,21 +33,21 @@ public class EnviarMensaje extends SimpleBehaviour {
                 ACLMessage msg = new ACLMessage(0);
                 msg.addReceiver(new AID("agente-receptor", AID.ISLOCALNAME));
                 msg.setContent("Hola receptor");
-                msg.setConversationId("identificador-conversacion");
+                msg.setConversationId(this.idConversacion);
                 this.agente.send(msg);
                 this.paso = 1;
                 break;
             case 1: // El emisor espera respuesta del receptor
                 System.out.println("[Emisor] Paso 2 - Espero respuesta de emisor");
                 ACLMessage reply = this.agente.blockingReceive();
-                if (reply.getConversationId().equals("identificador-conversacion")) {
+                if (reply.getConversationId().equals(this.idConversacion)) {
                     System.out.println("[Emisor] Mensaje recibido: " + reply.getContent());
                 }
                 else {
                     System.err.println("Error: ids no coinciden");
                     this.agente.doDelete();
                 }
-                this.paso = 2;
+                this.finalizado = true;
                 break;
             default:
                 System.err.println("Error en el protocolo");
@@ -52,14 +56,60 @@ public class EnviarMensaje extends SimpleBehaviour {
         }
     }
 
+    private void envioPerformativa() {
+        ACLMessage msg, reply;
+        switch (this.paso) {
+            case 0:
+                msg = new ACLMessage(ACLMessage.REQUEST);
+                msg.addReceiver(new AID("agente-receptor", AID.ISLOCALNAME)); // !!!
+                msg.setConversationId(this.idConversacion);
+                this.agente.send(msg);
+                System.out.println("[Emisor] Envio REQUEST al receptor");
+                this.paso = 1;
+                break;
+            case 1:
+                msg = this.agente.blockingReceive();
+                if (msg.getConversationId().equals(this.idConversacion) && msg.getPerformative() == ACLMessage.AGREE) {
+                    System.out.println("[Emisor] AGREE del receptor recibido");
+                    reply = msg.createReply(ACLMessage.INFORM);
+                    reply.setContent("He recibido tu agree, receptor!");
+                    this.agente.send(reply);
+                    System.out.println("[Emisor] Envio INFORM a receptor");
+                    this.paso = 2;
+                }
+                else {
+                    System.err.println("[Emisor] Error en el protocolo");
+                    this.agente.doDelete();
+                }
+                break;
+            case 2:
+                msg = this.agente.blockingReceive();
+                if (msg.getConversationId().equals(this.idConversacion) && msg.getPerformative() == ACLMessage.INFORM) {
+                    System.out.println("[Emisor] INFORM del receptor recibido: " + msg.getContent());
+                    this.finalizado = true;
+                }
+                else {
+                    System.err.println("[Emisor] Error en el protocolo");
+                    this.agente.doDelete();
+                }
+            default:
+                System.err.println("[Emisor] Error en el protocolo");
+                this.agente.doDelete();
+                break;
+        }
+    }
+
     @Override
     public void action() {
-        envioPorPasos();
+        envioPerformativa();
     }
 
     @Override
     public boolean done() {
-        return paso == 2;
+        if (finalizado) {
+            System.out.println("EnviarMensaje finaliza");
+        }
+        return finalizado;
     }
 
 }
--- P3/tutorial/RecibirMensaje.java
+++ P3/tutorial/RecibirMensaje.java
@@ -6,9 +6,13 @@ import jade.lang.acl.ACLMessage;
 public class RecibirMensaje extends SimpleBehaviour {
     
     private AgenteReceptor agente;
+    private int paso;
+    private boolean finalizado;
 
     RecibirMensaje(AgenteReceptor agente) {
         this.agente = agente;
+        this.paso = 0;
+        this.finalizado = false;
     }
 
     private void recepcionSimple() {
@@ -28,14 +32,60 @@ public class RecibirMensaje extends SimpleBehaviour {
         this.agente.send(reply);
     }
 
+    private void recepcionPerformativa() {
+        ACLMessage msg, reply;
+        switch (this.paso) {
+            case 0:
+                System.out.println("[Receptor] Espero mensaje del emisor");
+                msg = this.agente.blockingReceive();
+                System.out.println(msg);
+                if (msg.getPerformative() == ACLMessage.REQUEST) {
+                    System.out.println("[Receptor] REQUEST del emisor recibido");
+                    reply = msg.createReply(ACLMessage.AGREE);
+                    this.agente.send(reply);
+                    System.out.println("[Receptor] Enviamos AGREE al emisor");
+                    this.paso = 1;
+                }
+                else {
+                    System.err.println("[Receptor] Error en el protocolo");
+                    this.agente.doDelete();
+                }
+                break;
+                
+            case 1:
+                System.out.println("[Receptor] Espero mensaje del emisor");
+                msg = this.agente.blockingReceive();
+                System.out.println(msg);
+                if (msg.getPerformative() == ACLMessage.INFORM) {
+                    System.out.println("[Receptor] INFORM del emisor recibido: " + msg.getContent());
+                    reply = msg.createReply(ACLMessage.INFORM);
+                    reply.setContent("Perfecto pues");
+                    this.agente.send(reply);
+                    this.finalizado = true;
+                }
+                else {
+                    System.err.println("[Receptor] Error en el protocolo");
+                    this.agente.doDelete();
+                }
+                break;
+            default:
+                System.err.println("[Receptor] Error en el protocolo");
+                this.agente.doDelete();
+                break;
+        }
+    }
+
     @Override
     public void action() {
-        recepcionPorPasos();
+        recepcionPerformativa();
     }
 
     @Override
     public boolean done() {
-        return true;
+        if (finalizado) {
+            System.out.println("RecibirMensaje finaliza");
+        }
+        return finalizado;
     }
 
 }
